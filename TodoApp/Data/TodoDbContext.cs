a1cae047b421293b57df2c2a545019583e3d3ba9
Ertugrul <ertugrul@Ertugruls-MacBook-Air.local>
2023-04-12 00:47:15 -0400

    db context class added

---
+++ TodoApp/Data/TodoDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;
+
+namespace TodoApp.Data
+{
+	public class TodoDbContext:DbContext
+	{
+		public DbSet<Todo> Todo { get; set; }
+        public TodoDbContext(DbContextOptions<TodoDbContext> options)
+         : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Todo>().HasData(new Todo { Title = "Play basketball", IsCompleted=true }) ;
+            modelBuilder.Entity<Todo>().HasData(new Todo { Title = "Shopping", IsCompleted = false });
+            modelBuilder.Entity<Todo>().HasData(new Todo { Title = "Assignment", IsCompleted = true });
+            modelBuilder.Entity<Todo>().HasData(new Todo { Title = "Workout", IsCompleted = false });
+            modelBuilder.Entity<Todo>().HasData(new Todo { Title = "Make food", IsCompleted = false });
+            modelBuilder.Entity<Todo>().HasData(new Todo { Title = "Cleaning", IsCompleted = true });
+
+        }
+    }
+}
+
