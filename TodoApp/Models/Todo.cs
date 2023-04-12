2972a32b50693acbd31b2c47daa58fa8e5dfc3d9
Ertugrul <ertugrul@Ertugruls-MacBook-Air.local>
2023-04-12 00:40:36 -0400

    todo model created

---
+++ TodoApp/Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace TodoApp.Models
+{
+	public class Todo
+	{
+        [Key]
+        public string Title { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
+
